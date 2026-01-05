4eb96b39f65fe1e59f60a159d149f52af9c646f0
tanapolg <tanapolg93@gmail.com>
2026-01-05 18:18:27 +0100

    Snake game as in Nokia 1100

---
+++ main.py
@@ -0,0 +1,59 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 23:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+
+
+screen.exitonclick()
+
+# for i in range(1, 4):
+#     seg = snake_body[-i]
+#     seg.forward(20)
+#     screen.update()
+#     time.sleep(1)
+
+
+# starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+# new_turtle.speed(0)
